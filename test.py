a97f37ece843bb3ea451225eaa9d807f728251f8
MalcolmJarrett <48919738+MalcolmJarrett@users.noreply.github.com>
2020-05-14 22:17:07 +0100

    New attemot at Chapter 4

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 print ("Test File created on my PC")
 print ("Then edited on GitHub")
 print ("Can we commit to a branch?")
-Changed on PC in new branch
-Changed on GitHub
-
+print("New for Chapter4")
